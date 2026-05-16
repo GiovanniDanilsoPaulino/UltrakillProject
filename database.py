ebe2563791e7f711aa1893590c63f0c6d936a75d
Giovanni Paulino <giovannidpaulino@gmail.com>
2026-05-16 01:18:38 -0400

    Added Enemies table

--- database.py
+++ database.py
@@ -20,5 +20,29 @@ mycursor.execute("CREATE TABLE test(" \
 
 mycursor.execute("SHOW TABLES")
 
+create_enemies = "CREATE TABLE Enemies(" \
+"name VARCHAR(50)," \
+"type VARCHAR(50)," \
+"data TEXT," \
+"strategy TEXT)"
+
+insert_enemies = "INSERT INTO Enemies(name, type, data, strategy)" \
+"VALUES" \
+"('Filth', 'Lesser Husk', 'fill data later', 'fill strategy later')," \
+"('Stray', 'Lesser Husk', 'fill data later', 'fill strategy later')," \
+"('Schism', 'Greater Husk', 'fill data later', 'fill stategy later')"
+
+select_enemies = "SELECT *" \
+"FROM terminal.enemies"
+
 for x in mycursor:
     print(x)
+
+mycursor.execute(create_enemies)
+mycursor.execute(insert_enemies)
+mycursor.execute(select_enemies)
+
+for x in mycursor:
+    print(x)
+
+
