73d0cc03b518e3d2a428c1321742357d17b106f3
Giovanni Paulino <giovannidpaulino@gmail.com>
2026-05-19 17:07:21 -0400

    Added all enemies and some data

---
+++ data.py
@@ -0,0 +1,183 @@
+husk_data = "Husks are physical manifestations of the souls of the damned." \
+"" \
+"The physical form is based on the value of the original soul, which is determined by the strength of its will and its prevalence in public consciousness: the living souls that remember it." \
+"" \
+"Filth are the lowest form of Husk, whose souls were too weak and unimportant to even form a complete physical body." \
+"" \
+"Even among Husks, they have the lowest intelligence, driven purely by hunger."
+
+husk_strategy = "Most weapons are easily capable of taking down Filth, but their powerful jaws and sheer numbers can overwhelm a target quickly if underestimated." \
+"" \
+"Explosives are the most effective way to take down swarms, but any weapon that can hit more than one target will be efficient."
+
+stray_data = "While their tall stature may seem intimidating, Strays are afraid of most danger and will try to stay at a safe distance, only attacking via projectiles formed with Hell energy." \
+"" \
+"Although controlling and manifesting this energy is a complicated task, Strays have very low intelligence and are only able to do so via pure instinct." \
+"" \
+"Nevertheless, humans were unable to replicate this level of accuracy and control, particularly the Stray\\'s ability to cause the energy orbs to selectively ignore other Husks."
+
+stray_strategy = "Most weapons will be effective against them, but a Revolver headshot is the quickest and surest way to eliminate a Stray." \
+"" \
+"Due to their static nature and slow rate of attacking, they are an excellent target for projectile parrying."
+
+schism_data = "The result of two souls attempting to manifest in the same space, causing an amalgamation of two physical bodies." \
+"" \
+"Due to the doubled body mass, they\\'re quite resilient to damage, but have very poor motor control and thus cannot aim with any degree of accuracy, resorting to barrages of energy orbs in the general direction of their opponent."
+
+schism_strategy = "Piercer revolver\\'s charged headshots are an effecient tool for a quick kill against Schisms, though their displaced head cna make aiming difficult for the inexperienced." \
+"" \
+"A point-blank Pump Charge shotgun blast with 2 pumps will be able to take them down in a single shot." \
+"" \
+"Due to their poor aim, they aren\\'t a priority target, but the sheer amount of projectiles can sometimes make avoiding them tricky in busy encounters."
+
+soldier_data = "Soldiers are an augmented version of Strays, whose technological implants have been scavenged from broken machines to channel Hell Energy with greater efficiency." \
+"" \
+"This increase in power gives Soldiers more self-confidence, causing them to act more aggressively than normal Strays." \
+"" \
+"Despite the enhancements, their intelligence remains low and it has yet to be determined who or what actually augmented them."
+
+soldier_strategy = "The shotgun is an excellent weapon against Soldiers, as long as its use is swift so as to not allow them to protect themselves with melee attacks." \
+"" \
+"Since Soldiers charge their shots in front of them rather than above them, the charge is also easier to interrupt via a precise revolver shot for an explosive kill." \
+"" \
+"Thanks to their augmentations, Soldiers are able to withstand normal explosions with little to no damage. This more powerful red explosions will go through their guard however, and they are unable to block if not on the ground."
+
+minos_data = "Once the great and beloved king of the Lust layer, Minos has now been reduced to a shambling corpse." \
+"" \
+"Due to his incredible power of will and status as a just ruler in life remembered even millenia after his death, the manifestation of his soul is the largest Husk to ever have been recorded." \
+"" \
+"Small traces of the original soul can still be detected in the body, but the corpse itself is animated and controlled entirely by the snakelike Parasites that he once commanded." \
+"" \
+"Despite once bringing upon the renaissance of the Lust layer, his corpse now only seeks sinners to punish."
+
+minos_strategy = "Due to his large stature it can be quite difficult to recover blood from him, but his hands are usually holding on to the walls of the arena, which can be used for refueling." \
+"" \
+"Any weapon will work against an enemy this large, but melee and projectile parries make for a quick way to inflict grave damage." \
+"" \
+"Although it moves slowly, the Black Hole he summons is extremely dangerous and cannot be destroyed."
+
+stalker_data = "As the Greed layer\\'s punishment, Stalkers have been forced to carry heavy boulders up the monuments of mankind\\'s greed for all eternity." \
+"" \
+"They have carried out their punishment for so long that their bodies have evolved, warped and grown to better suit it." \
+"" \
+"Their limbs have twisted to give them better balance while carrying boulders on their backs and their skin and muscles have completely dried up, allowing them to survive direct contact with the dunes of gold dust that Greed\\'s sun has raised to unfathomably high temperatures." \
+"" \
+"However, an unidentified sentient force has replaced the boulders they would normally carry with high tech bombs that, upon detonation, will transform any nearby blood into the gold dust \\\"sand\\\" that covers the layer\\'s surface." \
+"" \
+"Research has shown the technology to be very similar to the augmentations of the modified Strays known as Soldiers, so it is likely both modifications come from the same source."
+
+stalker_strategy = "The lights on the canister give information on a Stalker\\'s state. The color shows the state of their explosive and the brightness of the color shows how much health the Stalker has left." \
+"" \
+"It\\'s advisable to forcibly detonate a Stalker rather than allowing one to detonate itself, as the latter explosion will have a larger area of effect." \
+"" \
+"If a Stalker gets close to its target and can no longer be pushed away, a ground slam wave can launch them out of range so they can be detonated safely in mid-air, since the explosion is mostly horizontal." \
+"" \
+"Attaching magnets to a Stalker will reduce the radius of its detonation."
+
+insurrectionist_data = ""
+
+insurrectionist_strategy = ""
+
+ferryman_data = ""
+
+ferryman_strategy = ""
+
+reaper_data = ""
+
+reaper_strategy = ""
+
+swordsmachine_data = ""
+
+swordsmachine_strategy = ""
+
+drone_data = ""
+
+drone_strategy = ""
+
+streetcleaner_data = ""
+
+streetcleaner_strategy = ""
+
+v20_data = ""
+
+v20_strategy = ""
+
+mindflayer_data = ""
+
+mindflayer_strategy = ""
+
+v21_data = ""
+
+v21_strategy = ""
+
+sentry_data = ""
+
+sentry_strategy = ""
+
+gutterman_data = ""
+
+gutterman_strategy = ""
+
+guttertank_data = ""
+
+guttertank_strategy = ""
+
+earthmover_data = ""
+
+earthmover_strategy = ""
+
+face_data = ""
+
+face_strategy = ""
+
+cerberus_data = ""
+
+cerberus_strategy = ""
+
+mass_data = ""
+
+mass_strategy = ""
+
+idol_data = ""
+
+idol_strategy = ""
+
+leviathan_data = ""
+
+leviathan_strategy = ""
+
+mannequin_data = ""
+
+mannequin_strategy = ""
+
+minotaur_data = ""
+
+minotaur_strategy = ""
+
+deathcatcher_data = ""
+
+deathcatcher_strategy = ""
+
+geryon_data = ""
+
+geryon_strategy = ""
+
+judge_data = ""
+
+judge_strategy = ""
+
+virtue_data = ""
+
+virtue_strategy = ""
+
+apostate_data = ""
+
+apostate_strategy = ""
+
+providence_data = ""
+
+providence_strategy = ""
+
+power_data = ""
+
+power_strategy = ""
--- database.py
+++ database.py
@@ -1,6 +1,8 @@
 import mysql.connector
 import credentials
+import data
 
+#connects to database
 database = mysql.connector.connect(
     host = "localhost",
     user = credentials.username,
@@ -11,26 +13,54 @@ print(database)
 
 mycursor = database.cursor()
 
+#Creates terminal database
 mycursor.execute("DROP DATABASE IF EXISTS terminal")
 mycursor.execute("CREATE DATABASE terminal")
 mycursor.execute("USE terminal")
 
-mycursor.execute("CREATE TABLE test(" \
-"id INT NOT NULL, name VARCHAR(50))")
-
-mycursor.execute("SHOW TABLES")
-
+#Creates Enemies table
 create_enemies = "CREATE TABLE Enemies(" \
 "name VARCHAR(50)," \
 "type VARCHAR(50)," \
 "data TEXT," \
 "strategy TEXT)"
 
+#Inserts all of the enemies information into the Enemies table
 insert_enemies = "INSERT INTO Enemies(name, type, data, strategy)" \
 "VALUES" \
-"('Filth', 'Lesser Husk', 'fill data later', 'fill strategy later')," \
-"('Stray', 'Lesser Husk', 'fill data later', 'fill strategy later')," \
-"('Schism', 'Greater Husk', 'fill data later', 'fill stategy later')"
+"('Filth', 'Lesser Husk', '" + data.husk_data + "', '" + data.husk_strategy + "')," \
+"('Stray', 'Lesser Husk', '" + data.stray_data + "', '" + data.stray_strategy + "')," \
+"('Schism', 'Greater Husk', '" + data.schism_data + "', '" + data.schism_strategy + "')," \
+"('Soldier', 'Greater Husk', '" + data.soldier_data + "', '" + data.soldier_strategy + "')," \
+"('The Corpse of King Minos', 'Supreme Husk', '" + data.minos_data + "', '" + data.minos_strategy + "')," \
+"('Stalker', 'Lesser Husk', '" + data.stalker_data + "', '" + data.stalker_strategy + "')," \
+"('Insurrectionist', 'Supreme Husk', '" + data.insurrectionist_data + "', '" + data.insurrectionist_strategy + "')," \
+"('Ferryman', 'Supreme Husk', '" + data.ferryman_data + "', '" + data.ferryman_strategy + "')," \
+"('Mirror Reaper', 'Supreme Husk', '" + data.reaper_data + "', '" + data.reaper_strategy + "')," \
+"('Swordsmachine', 'Greater Machine', '" + data.swordsmachine_data + "', '" + data.swordsmachine_strategy + "')," \
+"('Drone', 'Lesser Machine', '" + data.drone_data + "', '" + data.drone_strategy + "')," \
+"('Streetcleaner', 'Lesser Machine', '" + data.streetcleaner_data + "', '" + data.streetcleaner_strategy + "')," \
+"('V2', 'Supreme Machine', '" + data.v20_data + "', '" + data.v20_strategy + "')," \
+"('Mindflayer', 'Greater Machine', '" + data.mindflayer_data + "', '" + data.mindflayer_strategy + "')," \
+"('V2 \\(2nd\\)', 'Supreme Machine', '" + data.v21_data + "', '" + data.v21_strategy + "')," \
+"('Sentry', 'Greater Machine', '" + data.sentry_data + "', '" + data.sentry_strategy + "')," \
+"('Gutterman', 'Greater Machine', '" + data.gutterman_data + "', '" + data.gutterman_strategy + "')," \
+"('Guttertank', 'Greater Machine', '" + data.guttertank_data + "', '" + data.guttertank_strategy + "')," \
+"('Earthmover', 'Supreme Machine', '" + data.earthmover_data + "', '" + data.earthmover_strategy + "')," \
+"('Malicious Face', 'Lesser Demon', '" + data.face_data + "', '" + data.face_strategy + "')," \
+"('Cerberus', 'Lesser Demon', '" + data.cerberus_data + "', '" + data.cerberus_strategy + "')," \
+"('Hideous Mass', 'Greater Demon', '" + data.mass_data + "', '" + data.mass_strategy + "')," \
+"('Idol', 'Lesser Demon', '" + data.idol_data + "', '" + data.idol_strategy + "')," \
+"('Leviathan', 'Supreme Demon', '" + data.leviathan_data + "', '" + data.leviathan_strategy + "')," \
+"('Mannequin', 'Lesser Demon', '" + data.mannequin_data + "', '" + data.mannequin_strategy + "')," \
+"('Minotaur', 'Supreme Demon', '" + data.minotaur_data + "', '" + data.minotaur_strategy + "')," \
+"('Deathcatcher', 'Lesser Demon', '" + data.deathcatcher_data + "', '" + data.deathcatcher_strategy + "')," \
+"('Geryon', 'Supreme Demon', '" + data.geryon_data + "', '" + data.geryon_strategy + "')," \
+"('Gabriel, Judge of Hell', 'Supreme Angel', '" + data.judge_data + "', '" + data.judge_strategy + "')," \
+"('Virtue', 'Lesser Angel', '" + data.virtue_data + "', '" + data.virtue_strategy + "')," \
+"('Gabriel, Apostate of Hate', 'Supreme Angel', '" + data.apostate_data + "', '" + data.apostate_strategy + "')," \
+"('Providence', 'Lesser Angel', '" + data.providence_data + "', '" + data.providence_strategy + "')," \
+"('Power', 'Greater Angel', '" + data.power_data + "', '" + data.power_strategy + "')"
 
 select_enemies = "SELECT *" \
 "FROM terminal.enemies"
@@ -40,9 +70,11 @@ for x in mycursor:
 
 mycursor.execute(create_enemies)
 mycursor.execute(insert_enemies)
-mycursor.execute(select_enemies)
 
+#Test that prints all enemies information
+mycursor.execute(select_enemies)
 for x in mycursor:
     print(x)
 
-
+#def selectType(string type) :
+#    a = type
